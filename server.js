95e9772a02a56d0aa4955b5e89fd16a0be402957
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-12 18:59:50 -0600

    feat: add route for adding an exercise, clean up old code

--- server.js
+++ server.js
@@ -1,98 +1,157 @@
-require("dotenv").config()
-const express = require("express")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { formatDate, isValidDate } = require("./utils");
+const app = express();
 
 // set up database
-mongoose.connect(process.env.MONGOLAB_URI)
-const Schema = mongoose.Schema
+mongoose.connect(process.env.MONGOLAB_URI);
+const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true }
-})
-const UserModel = mongoose.model("User", userSchema)
+});
+const exerciseSchema = new Schema({
+  userId: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, required: false }
+});
+const UserModel = mongoose.model("User", userSchema);
+const ExerciseModel = mongoose.model("Exercise", exerciseSchema);
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(express.static("public"))
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html")
-})
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 // route for creating a new user
 app.post("/api/exercise/new-user", (req, res) => {
-  const user = req.body.username
+  const { username } = req.body;
   const newUser = new UserModel({
-    username: user
-  })
+    username
+  });
 
-  if (user === "") {
-    res.send("You must enter a username.")
-  } else {
-    newUser.save((err, newUser) => {
-      if (err) {
-        if (err.code === 11000) {
-          // username already in db
-          res.send("Duplicate username, please try another.")
-        } else {
-          res.send("Error saving username to database.")
-        }
-        return console.error(err)
-      } else {
-        // we're good, save the unique username
-        res.json(newUser)
-        return console.log("successfully saved username", user)
-      }
-    })
+  if (username === "") {
+    return res.send("You must enter a username.");
   }
-})
+
+  newUser.save((err, newUser) => {
+    if (err) {
+      console.log(err);
+      if (err.code === 11000) {
+        // username already in db
+        return res.send("Duplicate username, please try another.");
+      }
+      return res.send("Error saving username to database.");
+    }
+    console.log(
+      `successfully saved username ${username} with ID ${newUser._id}`
+    );
+    return res.json(newUser);
+  });
+});
 
 // route for getting all users
 app.get("/api/exercise/users", async (req, res) => {
   try {
-    const data = await UserModel.find({})
-    res.status(200).json(data)
+    const data = await UserModel.find({});
+    res.status(200).json(data);
   } catch (err) {
-    console.error("Error getting all users from database")
-    throw err
+    console.error("Error getting all users from database");
+    throw err;
   }
-})
+});
 
 // route for creating a new exercise
-app.post("/api/exercise/add", (req, res) => {})
+app.post("/api/exercise/add", (req, res) => {
+  let convertedDate;
+  const { userId, description, duration, date } = req.body;
+  // make sure all required fields aren't empty
+  const requiredFields = [userId, description, duration];
+  if (requiredFields.includes("")) {
+    return res.send("Please enter all required fields.");
+  }
+  // validate date
+  if (date === "") {
+    convertedDate = new Date();
+  } else {
+    if (isValidDate(date)) {
+      convertedDate = date;
+    } else {
+      return res.send("Please send date in YYYY-MM-DD format only.");
+    }
+  }
+
+  // validate userid
+  UserModel.findOne({ _id: userId }, (err, user) => {
+    if (err) {
+      console.log(err);
+      return res.send("Error finding user ID.");
+    }
+    if (user === null) {
+      return res.send("Could not find user with this ID.");
+    }
+    const newExercise = new ExerciseModel({
+      userId,
+      description,
+      duration,
+      date: convertedDate
+    });
+
+    newExercise.save((err, newExercise) => {
+      if (err) {
+        console.log(err);
+        return res.send("Error saving exercise to database.");
+      }
+    });
+
+    console.log(`successfully saved exercise with ID ${newExercise._id}`);
+    return res.json(newExercise);
+  });
+});
 
 // route for getting an exercise
-app.get("/api/exercise/:log", (req, res) => {})
+app.get("/api/exercise/log", (req, res) => {
+  /**
+   * I can retrieve a full exercise log of any user by getting /api/exercise/log with a parameter of userId(_id). App will return the user object with added array log and count (total exercise count).
+   *
+   * I can retrieve part of the log of any user by also passing along optional parameters of from & to or limit. (Date format yyyy-mm-dd, limit = int)
+   */
+  // console.log(req.params);
+});
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({ status: 404, message: "not found" })
-})
+  return next({ status: 404, message: "not found" });
+});
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
-  let errCode, errMessage
+  let errCode, errMessage;
 
   if (err.errors) {
     // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
     // report the first validation error
-    errMessage = err.errors[keys[0]].message
+    errMessage = err.errors[keys[0]].message;
   } else {
     // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || "Internal Server Error"
+    errCode = err.status || 500;
+    errMessage = err.message || "Internal Server Error";
   }
   res
     .status(errCode)
     .type("txt")
-    .send(errMessage)
-})
+    .send(errMessage);
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log("Your app is listening on port " + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
---
+++ utils.js
@@ -0,0 +1,22 @@
+const formatDate = date => {
+  const d = new Date(date);
+  let month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [year, month, day].join("-");
+};
+
+const isValidDate = dateString => {
+  const regEx = /^\d{4}-\d{2}-\d{2}$/;
+  if (!dateString.match(regEx)) return false; // Invalid format
+  const d = new Date(dateString);
+  const dNum = d.getTime();
+  if (!dNum && dNum !== 0) return false; // NaN value, Invalid date
+  return d.toISOString().slice(0, 10) === dateString;
+};
+
+module.exports = { formatDate, isValidDate };
